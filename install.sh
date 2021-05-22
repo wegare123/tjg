d96fe0f54df74f4ff2e36d1e515b233d5e49750d
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-05-22 15:12:18 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#tjg (Wegare)
+printf 'ctrl+c' | crontab -e > /dev/null
+opkg update && opkg install unzip
+cek=$(cat /etc/openwrt_r*)
+if [[ $cek == *"LEDE"* ]] && [[ $cek == *"ar71xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/ar71xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ar71xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ar71xx
+elif [[ $cek == *"LEDE"* ]] && [[ $cek == *"brcm63xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/brcm63xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/brcm63xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/brcm63xx
+elif [[ $cek == *"LEDE"* ]] && [[ $cek == *"ramips"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/ramips.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ramips/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ramips
+elif [[ $cek == *"LEDE"* ]] && [[ $cek == *"sunxi"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/sunxi.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/sunxi/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/sunxi
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"ar71xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/ar71xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ar71xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ar71xx
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"brcm63xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/brcm63xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/brcm63xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/brcm63xx
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"ramips"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/ramips.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ramips/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ramips
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"sunxi"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/sunxi.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/sunxi/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/sunxi
+elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"aarch64_cortex-a53"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/sunxi.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/sunxi/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/sunxi
+elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"mips_24kc"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/ar71xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ar71xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ar71xx
+elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"mipsel_24kc"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/ramips.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ramips/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ramips
+else
+echo -e "version anda tidak terdeteksi!"
+exit
+fi
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/tjg/main/tjg.sh" -O /usr/bin/tjg
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/tjg/main/trojan" -O /usr/bin/trojan
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/tjg/main/autorekonek-tjg.sh" -O /usr/bin/autorekonek-tjg
+opkg install ip-full ipset lsof fping && opkg install *.ipk
+chmod +x /usr/bin/tjg
+chmod +x /usr/bin/autorekonek-tjg
+rm -r ~/*.ipk
+rm -r ~/install.sh
+mkdir -p ~/akun/
+touch ~/akun/tjg.txt
+sleep 2
+echo "install selesai"
+echo "untuk memulai tools silahkan jalankan perintah 'tjg'"
+#echo "silahkan reboot terlebih dahulu"
+
+
+				
---
+++ tjg.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+#tjg (Wegare)
+clear
+host2="$(cat /root/akun/tjg.txt | tr '\n' ' '  | awk '{print $1}')" 
+port2="$(cat /root/akun/tjg.txt | tr '\n' ' '  | awk '{print $2}')" 
+bug2="$(cat /root/akun/tjg.txt | tr '\n' ' '  | awk '{print $3}')" 
+pas2="$(cat /root/akun/tjg.txt | tr '\n' ' '  | awk '{print $4}')" 
+path2="$(cat /root/akun/tjg.txt | tr '\n' ' '  | awk '{print $5}')" 
+udp2="$(cat /root/akun/tjg.txt | tr '\n' ' '  | awk '{print $6}')" 
+
+echo "Inject trojan-go by wegare"
+echo "1. Sett Profile"
+echo "2. Start Inject"
+echo "3. Stop Inject"
+echo "4. Enable auto booting & auto rekonek"
+echo "5. Disable auto booting & auto rekonek"
+echo "e. exit"
+read -p "(default tools: 2) : " tools
+[ -z "${tools}" ] && tools="2"
+if [ "$tools" = "1" ]; then
+
+echo "Masukkan host/ip" 
+read -p "default host/ip: $host2 : " host
+[ -z "${host}" ] && host="$host2"
+
+echo "Masukkan port" 
+read -p "default port: $port2 : " port
+[ -z "${port}" ] && port="$port2"
+
+echo "Masukkan password/key" 
+read -p "default password/key: $pass2 : " pass
+[ -z "${pass}" ] && pass="$pass2"
+
+echo "Masukkan bug" 
+read -p "default bug: $bug2 : " bug
+[ -z "${bug}" ] && bug="$bug2"
+
+read -p "ingin menggunakan port udpgw y/n " pilih
+if [ "$pilih" = "y" ]; then
+echo "Masukkan port udpgw" 
+read -p "default udpgw: $udp2 : " udp
+[ -z "${udp}" ] && udp="$udp2"
+badvpn="--socks-server-addr 127.0.0.1:1080 --udpgw-remote-server-addr 127.0.0.1:$udp"
+elif [ "$pilih" = "Y" ]; then
+echo "Masukkan port udpgw" 
+read -p "default udpgw: $udp2 : " udp
+[ -z "${udp}" ] && udp="$udp2"
+badvpn="--socks-server-addr 127.0.0.1:1080 --udpgw-remote-server-addr 127.0.0.1:$udp"
+else
+badvpn="--socks-server-addr 127.0.0.1:1080"
+fi
+if [[ -z $udp ]]; then
+udp="-"
+fi
+echo "$host
+$port
+$pass
+$bug
+$path
+$udp" > /root/akun/tjg.txt
+cat <<EOF> /root/akun/tjg.json
+{
+  "run_type": "client",
+  "local_addr": "127.0.0.1",
+  "local_port": 1080,
+  "remote_addr": "$host",
+  "remote_port": $port,
+  "password": ["$pass"]
+  "websocket": {
+    "enabled": true,
+    "path": "$path",
+    "hostname": "$bug"
+   }
+}
+
+EOF
+cat <<EOF> /usr/bin/gproxy-tjg
+badvpn-tun2socks --tundev tun1 --netif-ipaddr 10.0.0.2 --netif-netmask 255.255.255.0 $badvpn --udpgw-connection-buffer-size 65535 --udpgw-transparent-dns &
+EOF
+chmod +x /usr/bin/gproxy-tjg
+echo "Sett Profile Sukses"
+sleep 2
+clear
+/usr/bin/tjg
+elif [ "${tools}" = "2" ]; then
+ipmodem="$(route -n | grep -i 0.0.0.0 | head -n1 | awk '{print $2}')" 
+echo "ipmodem=$ipmodem" > /root/akun/ipmodem.txt
+udp="$(cat /root/akun/tjg.txt | tr '\n' ' '  | awk '{print $6}')" 
+host="$(cat /root/akun/tjg.txt | tr '\n' ' '  | awk '{print $1}')" 
+route="$(cat /root/akun/ipmodem.txt | grep -i ipmodem | cut -d= -f2 | tail -n1)"
+
+trojan-go -c /root/akun/tjg.json &
+sleep 3
+ip tuntap add dev tun1 mode tun
+ifconfig tun1 10.0.0.1 netmask 255.255.255.0
+/usr/bin/gproxy-tjg
+route add 8.8.8.8 gw $route metric 0
+route add 8.8.4.4 gw $route metric 0
+route add $host gw $route metric 0
+route add default gw 10.0.0.2 metric 0
+echo "
+#!/bin/bash
+#tjg (Wegare)
+host=$(cat /root/akun/tjg.txt | tr '\n' ' '  | awk '{print $1}')
+fping -l $host" > /usr/bin/ping-tjg
+chmod +x /usr/bin/ping-tjg
+/usr/bin/ping-tjg > /dev/null 2>&1 &
+sleep 5
+elif [ "${tools}" = "3" ]; then
+host="$(cat /root/akun/tjg.txt | tr '\n' ' '  | awk '{print $1}')" 
+route="$(cat /root/akun/ipmodem.txt | grep -i ipmodem | cut -d= -f2 | tail -n1)" 
+#killall screen
+killall -q badvpn-tun2socks trojan ping-tjg fping
+route del 8.8.8.8 gw "$route" metric 0 2>/dev/null
+route del 8.8.4.4 gw "$route" metric 0 2>/dev/null
+route del "$host" gw "$route" metric 0 2>/dev/null
+ip link delete tun1 2>/dev/null
+killall dnsmasq 
+/etc/init.d/dnsmasq start > /dev/null
+sleep 2
+echo "Stop Suksess"
+sleep 2
+clear
+/usr/bin/tjg
+elif [ "${tools}" = "4" ]; then
+cat <<EOF>> /etc/crontabs/root
+
+# BEGIN AUTOREKONEKTJG
+*/1 * * * *  autorekonek-tjg
+# END AUTOREKONEKTJG
+EOF
+sed -i '/^$/d' /etc/crontabs/root 2>/dev/null
+/etc/init.d/cron restart
+echo "Enable Suksess"
+sleep 2
+clear
+/usr/bin/tjg
+elif [ "${tools}" = "5" ]; then
+sed -i "/^# BEGIN AUTOREKONEKTJG/,/^# END AUTOREKONEKTJG/d" /etc/crontabs/root > /dev/null
+/etc/init.d/cron restart
+echo "Disable Suksess"
+sleep 2
+clear
+/usr/bin/tjg
+elif [ "${tools}" = "e" ]; then
+clear
+exit
+else 
+echo -e "$tools: invalid selection."
+exit
+fi
